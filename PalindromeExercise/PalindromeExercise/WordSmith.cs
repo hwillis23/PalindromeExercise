7733750c69cef3fb20b0e2eef037a4eff0fdbd78
Hannah Willis <hwillis8817@gmail.com>
2022-04-11 14:44:15 -0500

    inital commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reverse = "";
+
+            for (int i = word.Length -1; i >= 0 ; i--)
+            {
+                reverse += word.ToLower()[i];
+            }
+
+            if (reverse == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("Hannah",true)]
+        [InlineData("Bob",true)]
+        [InlineData("Alice", false)]
+        public void NameTester(string word,bool expected )
+        { 
+
+            //Arrange 
+            WordSmith tester = new WordSmith();
+
+            //Act 
+           var actual = tester.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+               
 
         }
     }
